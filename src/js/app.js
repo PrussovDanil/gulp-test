5aa6d1dc7063d07f14d3bddac5cab2ccc98b439d
Danil Prussov <danilprussov@gmai.com>
2022-05-24 20:38:00 +0600

    feat(finish but not yet ): not yet

--- src/js/app.js
+++ src/js/app.js
@@ -4,5 +4,22 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper();
-console.log('hello');
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  
+  
+  silulateTouch: true,
+  touchRatio:1,
+  touchAngle:45,
+  grabCursor:true,
+  slidesPerView:3,
+  spaceBetween: 35,
+  centeredSlides: true,
+  initialSlide:1,
+  width:1600,
+ 
+  
+});
+
+console.log("hello");
